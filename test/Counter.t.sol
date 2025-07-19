a51ff387d5127f36e15623a06789458a49098499
AJTECH0001 <aladejamiudamilola@gmail.com>
2025-07-19 23:09:11 +0100

    Fix formatting issues for forge fmt

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,9 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test} from "forge-std/Test.sol" ;
+import {Test} from "forge-std/Test.sol";
 import {stdError} from "forge-std/StdError.sol";
 
-contract ContractBTest is Test {
- 
-}
+contract ContractBTest is Test {}
