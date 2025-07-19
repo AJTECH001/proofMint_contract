6e98729256ba3e9794c5038a5059732c75e68bbe
AJTECH0001 <aladejamiudamilola@gmail.com>
2025-07-19 23:01:03 +0100

    setup proofmint_contract file

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -5,18 +5,5 @@ import {Test} from "forge-std/Test.sol" ;
 import {stdError} from "forge-std/StdError.sol";
 
 contract ContractBTest is Test {
-    uint256 testNumber;
-
-    function setUp() public {
-        testNumber = 43; 
-    }
-
-    function test_NumberIs42() public view {
-        assertEq(testNumber, 43);
-    }
-
-    // function testRevert_Subtract43() public {
-    //     vm.
-    //     testNumber -=   43;
-    // }
+ 
 }
